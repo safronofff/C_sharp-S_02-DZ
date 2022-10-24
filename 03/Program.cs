7ce112b44a6c4c35a968e797689df74b552826c8
ys-off <yuriy.safronoff@yandex.ru>
2022-10-24 19:18:39 +0300

    03

---
+++ 03/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.WriteLine("Введите день недели");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool Vihodnoy(int x)
+{
+	if (x % 6 == 0 || x % 7 == 0) return true;
+	else return false;
+}
+
+bool y1 = Vihodnoy(num);
+Console.WriteLine(y1);
